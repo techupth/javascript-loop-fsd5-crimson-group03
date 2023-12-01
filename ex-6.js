1dc58df953663d345a72e2f67fb599b9143a33b1
CharlieHii <charlie_chaper@hotmail.com>
2023-12-01 13:46:52 +0700

    charlie

--- ex-6.js
+++ ex-6.js
@@ -4,4 +4,8 @@ let reversedCompanyName = "";
 
 // Start coding here
 
+for (let letter of companyName) {
+  reversedCompanyName = letter + reversedCompanyName;
+}
+
 console.log(reversedCompanyName);
